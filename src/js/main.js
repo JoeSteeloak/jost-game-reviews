768065bfc4964eb371e8fe4e79321e27860fda12
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2024-03-07 10:31:23 +0100

    add mash up

--- src/js/main.js
+++ src/js/main.js
@@ -14,11 +14,11 @@ const options1 = {
     }
 };
 const options2 = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7dd4cb536emshace755aa949c97bp16ae4ajsn31b792268097',
-		'X-RapidAPI-Host': 'steam-api7.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '7dd4cb536emshace755aa949c97bp16ae4ajsn31b792268097',
+        'X-RapidAPI-Host': 'steam-api7.p.rapidapi.com'
+    }
 };
 
 
@@ -38,13 +38,11 @@ async function fetchGame() {
         const response = await fetch(url1, options1);
         const result = await response.json();
 
-        console.log(result);
-
         /* Ladda in namn och skapa en div för varje resultat */
         result.forEach(e => {
             const gameName = e.name;
             const gameId = e.id;
-            /*  gameResultEl.innerHTML += `<div id=${gameId}><h1>${gameName}</h1></div>`; */
+
             const gameContainer = document.createElement("div");
             gameContainer.id = gameId;
             const gameNameEl = document.createElement("h1");
@@ -64,9 +62,7 @@ async function fetchGame() {
 // funktion för att visa spelet och recensionerna
 async function showReview(gameId, gameName) {
     const urlReview = `https://opencritic-api.p.rapidapi.com/reviews/game/${gameId}?skip=3&sort=newest`;
-    const urlSteamGameId = `https://steam-api7.p.rapidapi.com/search?query=${gameName}&limit=3`;
-    
-
+    const urlSteamGameId = `https://steam-api7.p.rapidapi.com/search?query=${gameName}&limit=1`;
 
 
     try {
@@ -78,18 +74,12 @@ async function showReview(gameId, gameName) {
         /* mash up för att hämta gameId */
         const response1 = await fetch(urlSteamGameId, options2);
         const result1 = await response1.json();
-        console.log(gameName);
-        console.log(result1);
-        console.log(result1[0].appid);
 
-        const urlScreenshots = `https://steam-api7.p.rapidapi.com/media/screenshots/${result1[0].appid}`;
+        const urlScreenshots = `https://steam-api7.p.rapidapi.com/media/screenshots/${result1.results[0].appid}`;
 
         /* fetch screenshots från API2 */
         const response2 = await fetch(urlScreenshots, options2);
         const result2 = await response2.json();
-
-        /* testa */
-        console.log(result);
 
         /* skapa screenshots */
         const screenshotEl = document.createElement('img');
@@ -102,7 +92,8 @@ async function showReview(gameId, gameName) {
             const reviewScore = result[i].score;
             const reviewOutlet = result[i].Outlet.name;
             const reviewSnippet = result[i].snippet;
-            gameReviewEl.innerHTML += `<div><h1>${reviewTitle}</h1><p>${reviewSnippet}</p><h2>${reviewScore}</h2><p>${reviewOutlet}</p></div>`;
+            const reviewUrl = result[i].externalUrl;
+            gameReviewEl.innerHTML += `<div><h1>${reviewTitle}</h1><p>${reviewSnippet}</p><h2>${reviewScore}</h2><p>Published on:${reviewOutlet}</p><a href=${reviewUrl}>Läs mer</a></div>`;
         }
 
     } catch (error) {
